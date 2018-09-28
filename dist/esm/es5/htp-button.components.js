f993a163bd7056b2093e56831e18016abc35a7c6
Andrew Walpole <walpolea@gmail.com>
2018-09-28 14:07:47 -0700

    new build

--- dist/esm/es5/htp-button.components.js
+++ dist/esm/es5/htp-button.components.js
@@ -1,3 +1,3 @@
 // HtpButton: Host Data, ES Module/ES5 Target
 
-export var HTPButton = ["htp-button",function(o){return(o.scoped?import("./bat22os0.sc.js"):import("./bat22os0.js")).then(function(m){return m.HtpButton})},1,[["curHoldTime",5],["el",7],["holdProgress",5],["holdTime",1,0,"hold-time",4],["holdTimeInterval",5],["isHolding",5]],1];
+export var HTPButton = ["htp-button",function(o){return(o.scoped?import("./lwue7rfj.sc.js"):import("./lwue7rfj.js")).then(function(m){return m.HtpButton})},1,[["curHoldTime",5],["el",7],["holdProgress",5],["holdTime",1,0,"hold-time",4],["holdTimeInterval",5],["isHolding",5]],1];
