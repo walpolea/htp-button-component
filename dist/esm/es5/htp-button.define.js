719ea60f8fbe537ec9e074dfeb539f9ce24d3b78
Andrew Walpole <walpolea@gmail.com>
2018-09-28 13:59:12 -0700

    new build

--- dist/esm/es5/htp-button.define.js
+++ dist/esm/es5/htp-button.define.js
@@ -1,17 +1,11 @@
 // HtpButton: Custom Elements Define Library, ES Module/ES5 Target
 import { defineCustomElement } from './htp-button.core.js';
 import {
-  HTPButton,
-  MyComponent,
-  TodoItem,
-  TodoList
+  HTPButton
 } from './htp-button.components.js';
 
 export function defineCustomElements(window, opts) {
   defineCustomElement(window, [
-    HTPButton,
-    MyComponent,
-    TodoItem,
-    TodoList
+    HTPButton
   ], opts);
 }
